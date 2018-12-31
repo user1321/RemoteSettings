b4bc55d92605ad0a8513642243eeae372fcc8a4b
user1321 <33780612+user1321@users.noreply.github.com>
2018-12-31 22:27:19 +0200

    Exit if IsTerminateRemoteSettingsPath.txt exist

--- Air/TxRemoteSettings.sh
+++ Air/TxRemoteSettings.sh
@@ -5,7 +5,7 @@ cd /home/pi/cameracontrol/IPCamera/wifibroadcast-master_original/
 NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
 
 
-while true
+while ! [ -e /tmp/IsTerminateRemoteSettingsPath.txt ]
 do
 	echo "start tx -m 10 -u 9292 -p 91 $NICS_LIST \n"
 	./tx -m 10 -u 9292 -p 91 $NICS_LIST
