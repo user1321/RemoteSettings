5884d7d8472fff357c1392c6146099b67ea56d13
user1321 <33780612+user1321@users.noreply.github.com>
2019-01-01 19:37:14 +0200

    Update TerminateRemoteSettingsAir.sh

--- TerminateRemoteSettingsAir.sh
+++ TerminateRemoteSettingsAir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+
 ps -ef | nice grep "rx -c 127.0.0.1 -u 9393 -p 90" | nice grep -v grep | awk '{print $2}' | xargs kill -9
 ps -ef | nice grep "tx -m 10 -u 9292 -p 91" | nice grep -v grep | awk '{print $2}' | xargs kill -9
