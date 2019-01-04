3cafa4ddaaf2a052b38f34640bbcfe29fd3db453
user1321 <33780612+user1321@users.noreply.github.com>
2019-01-04 15:12:55 +0200

    Update TxForwardFromAndroid.sh

--- Ground/TxForwardFromAndroid.sh
+++ Ground/TxForwardFromAndroid.sh
@@ -17,23 +17,23 @@ if [ -e "/tmp/settings.sh" ]; then
 fi
 
 
-cd /home/pi/cameracontrol/IPCamera/wifibroadcast-master_original/
+cd /home/pi/cameracontrol/IPCamera/svpcom_wifibroadcast/
 
 NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
 
 while true
 do
-	echo "start tx -u 9090 -p 90 $NICS_LIST  (forward msg from android phone. UDP port 9090.\n"
+	echo "start wfb_tx -u 9090 -p 90 $NICS_LIST  (forward msg from android phone. UDP port 9090.\n"
 
     	if [ "$EncryptionOrRange" == "Range" ]; then
-		./tx -m 10 -u 9090 -t 2 -p 90 $NICS_LIST
+		./wfb_tx -m 10 -u 9090 -t 2 -p 90 -B 20 -M 0 $NICS_LIST
     	fi
 
     	if [ "$EncryptionOrRange" == "Encryption" ]; then
-            ./tx -m 10 -u 9090 -t 0 -p 90 $NICS_LIST
+            ./wfb_tx -m 10 -u 9090 -t 0 -p 90 -B 20 -M 0 $NICS_LIST
     	fi
 
 	NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
-	echo "start tx -u 9090 -p 90 down. Restating with:  $NICS_LIST \n"
+	echo "start wfb_tx -u 9090 -p 90 down. Restating with:  $NICS_LIST \n"
 	sleep 2
 done
