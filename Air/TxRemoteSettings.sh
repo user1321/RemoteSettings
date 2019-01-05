cefa1970e6defe9280b5dbdb6a07e7f86563d6a7
user1321 <33780612+user1321@users.noreply.github.com>
2019-01-05 18:40:14 +0200

    Update TxRemoteSettings.sh

--- Air/TxRemoteSettings.sh
+++ Air/TxRemoteSettings.sh
@@ -7,11 +7,11 @@ NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep 
 
 while ! [ -e /tmp/IsTerminateRemoteSettingsPath.txt ]
 do
-	echo "start wfb_tx -m 10 -u 9292 -p 91 -B 20 -M 0 $NICS_LIST \n"
-	./wfb_tx -m 10 -u 9292 -p 91 -B 20 -M 0 $NICS_LIST
+	echo "start wfb_tx -u 9292 -p 91 -B 20 -M 0 $NICS_LIST \n"
+	./wfb_tx -u 9292 -p 91 -B 20 -M 0 $NICS_LIST
 
 	NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
-        echo "wfb_tx -m 10 -u 9292 -p 91. Restating with:  $NICS_LIST \n"
+        echo "wfb_tx -u 9292 -p 91. Restating with:  $NICS_LIST \n"
         sleep 2
 done
 
