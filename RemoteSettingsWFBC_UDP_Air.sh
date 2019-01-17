385669c4e722f8d4588a070294a91946c99e14e9
user1321 <33780612+user1321@users.noreply.github.com>
2019-01-17 19:24:54 +0200

    Move start code to standalone file

--- RemoteSettingsWFBC_UDP_Air.sh
+++ RemoteSettingsWFBC_UDP_Air.sh
@@ -18,12 +18,6 @@ fi
 /home/pi/RemoteSettings/Air/TxRemoteSettings.sh &
 #sleep 0.5
 /home/pi/RemoteSettings/Air/RxRemoteSettings.sh &
-#sleep 0.5
-/home/pi/RemoteSettings/Air/TxBandSwitcher.sh &
-sleep 0.2
-/home/pi/RemoteSettings/Air/RxBandSwitcher.sh &
-sleep 0.2
-python3.5 /home/pi/RemoteSettings/BandSwitchAir.py &
 
 if [ "$EncryptionOrRange" == "Encryption" ]; then
 	/home/pi/RemoteSettings/Air/RxJoystick.sh &
